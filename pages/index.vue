19f0b834ad7fa19cbec1234081c4ae8fb02b764c
daisuke <dsaito85a@gmail.com>
2020-01-28 21:46:02 +0900

    feat: 個室を作成した

--- pages/index.vue
+++ pages/index.vue
@@ -2,18 +2,18 @@
   <div>
     <header>
       <h1>
-        <img src="~/assets/image/logo-everyones-toilet.png" alt="みんなのトイレ" />
+        <img src="/image/logo-everyones-toilet.png" alt="みんなのトイレ" />
       </h1>
     </header>
     <main>
       <ul class="everyones-toilet">
         <li class="toilet01">
-          <img src="~/assets/image/toilet-close-used.png" alt="個室1" />
+          <img src="/images/toilet-close-used.png" alt="個室1" />
         </li>
         <li class="toilet02"><nuxt-link to="/rooms/2" id="toilet2"></nuxt-link></li>
         <li class="toilet03"><nuxt-link to="/rooms/3" id="toilet3"></nuxt-link></li>
-        <li class="toilet04"><img src="~/assets/image/toilet-close-used.png" alt="個室4"></li>
-        <li class="toilet05"><img src="~/assets/image/malfunction.png" alt="個室5"></li>
+        <li class="toilet04"><img src="/images/toilet-close-used.png" alt="個室4"></li>
+        <li class="toilet05"><img src="/images/malfunction.png" alt="個室5"></li>
       </ul>
     </main>
     <footer>
@@ -47,14 +47,14 @@ export default {
 .everyones-toilet li a {
   display: block;
   height: 100%;
-  background: url(~assets/image/toilet-close.png) no-repeat ;
+  background: url(/images/toilet-close.png) no-repeat ;
   padding-bottom: 12px;
   -webkit-transition: all 0.2s;
   transition: all 0.2s;
 }
 .everyones-toilet li a:hover,
 .everyones-toilet li a:focus {
-  background-image: url(~assets/image/toilet-open.png);
+  background-image: url(/images/toilet-open.png);
 }
 
 @media all and (max-width: 980px) {
--- pages/rooms/3.vue
+++ pages/rooms/3.vue
@@ -1,112 +1,13 @@
 <template>
-<div>
-    <main class="room">
-        <section class="room-wall">
-        </section>
-        <section class="room-floor">
-            <img class="toilet" src="images/toilet03.png" alt="3番目の個室のトイレ">
-            <div class="room-floor-btn-area">
-                <a class="sns-btn active-btn"
-                    href="http://twitter.com/intent/tweet?text=3番目の個室に入りました%0a&hashtags=みんなのトイレ&url=https://everyones-toilet.tokyo"
-                    onclick="window.open(this.href, 'tweetwindow', 'width=550, height=450,personalbar=0,toolbar=0,scrollbars=1,resizable=1'); return false;">
-                    トイレ報告
-                </a>
-                <a href="./">トイレから出る</a>
-            </div>
-        </section>
-    </main>
-    <footer>
-        &copy;みんなのトイレ
-    </footer>
-    </div>
+  <div>
+      <room num="3" toilet-img="/images/toilet03.png" />
+  </div>
 </template>
-
 <script>
+import Room from '@/components/Room.vue'
 export default {
+    components: {
+        Room
+    }
 }
 </script>
-
-<style scoped>
-
-/* 個室 */
-.room-wall {
-  position: relative;
-  max-width: 800px;
-  height: 420px;
-  margin: 0 auto;
-  white-space:nowrap;
-  overflow: scroll;
-}
-
-#fukidashi{
-  position: relative;
-  padding: 0 8px;
-  height: 30px;
-  background: #f7dee6;
-  border-radius: 4px;
-  line-height: 30px;
-}
-
-#fukidashi span {
-  position: absolute;
-  bottom: -4px;
-  width: 8px;
-  height: 8px;
-  display: block;
-  background: #f7dee6;
-  transform: rotate(45deg);
-}
-
-input[type="text"] {
-  position: relative;
-  -webkit-appearance: none;
-  background: rgba(255, 255, 255, 0.5);
-  outline: none;
-  padding: 4px 8px;
-}
-
-.room-floor {
-  position: relative;
-  width: 100%;
-}
-
-.room-floor .toilet {
-  position: absolute;
-  left:50%;
-  top: -20px;
-  margin-left: -138px;
-}
-
-.room-floor .paper {
-  position: absolute;
-  left:50%;
-  top: -80px;
-  margin-left: -100px;
-}
-
-.room-floor-btn-area {
-  position: absolute;
-  right: 12px;
-  bottom:12px;
-}
-.room-floor-btn-area a {
-  display: block;
-  margin-bottom: 8px;
-  padding: 12px;
-  border: 4px solid #191919;
-  background: #fff;
-}
-.room-floor-btn-area a:last-child {
-  margin-bottom: 0px;
-}
-
-footer {
-  position: fixed;
-  bottom: 0;
-  width: 100%;
-  color: #fff;
-  font-size: 13px;
-  background-color: #191919;
-  line-height: 40px;
-}
-</style>
